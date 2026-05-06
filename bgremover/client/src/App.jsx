2b8b5f41722632e2cd6ff892c794ac09cb6d6cd6
brolittle786-lgtm <brolittle786@gmail.com>
2026-05-06 16:11:40 +0530

    Change axios.post to use await for API call

--- bgremover/client/src/App.jsx
+++ bgremover/client/src/App.jsx
@@ -65,7 +65,7 @@ export default function App() {
     try {
       const formData = new FormData()
       formData.append('image', originalFile)
-     axios.post('https://remove-bg-1-q6pi.onrender.com/api/remove-bg', formData, {
+    const res = await axios.post('https://remove-bg-1-q6pi.onrender.com/api/remove-bg', formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
         timeout: 60000,
       })
